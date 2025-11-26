d588c85a219462f83c0ae5537582daeccc559388
Slindex <david.echajaya@hotmail.com>
2025-11-26 01:48:25 -0500

    split nodes and txt to txt nodes fx added

--- src/helpers.py
+++ src/helpers.py
@@ -3,7 +3,7 @@ from textnode import TextNode, TextType
 from htmlnode import LeafNode
 
 
-def text_node_to_html_node(text_node: TextNode):
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
     if not isinstance(text_node, TextNode):
         raise TypeError("argument should be a TextNode object")
     
@@ -30,7 +30,7 @@ def text_node_to_html_node(text_node: TextNode):
         case _:
             raise Exception("Text Node has an invalid Text Type")
         
-def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:TextType):
+def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:TextType) -> list[TextNode]:
     new_nodes = []
 
     for node in old_nodes:
@@ -73,8 +73,97 @@ def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:Tex
 
     return new_nodes
 
-def extract_markdown_images(text:str):
+def extract_markdown_images(text:str) -> list[tuple]:
     return re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
 
-def extract_markdown_links(text:str):
+def extract_markdown_links(text:str) -> list[tuple]:
     return re.findall(r"(?<!\!)\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_link(old_nodes:list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type is not TextType.PLAIN:
+            new_nodes.append(node)
+            continue
+        
+        links = extract_markdown_links(node.text)
+        texts = re.split(r"(?<!\!)\[.*?\]\(.*?\)", node.text)
+
+        while "" in texts:
+            texts.remove("")
+
+        iters = max(len(links), len(texts))
+
+        for i in range(iters):
+            if node.text[0] == "[":
+                link_node = TextNode(links[i][0], TextType.LINK, links[i][1])
+                new_nodes.append(link_node)
+
+                try:
+                    text_node = TextNode(texts[i], TextType.PLAIN)
+                    new_nodes.append(text_node)
+                    continue
+                except IndexError:
+                    break
+            
+            text_node = TextNode(texts[i], TextType.PLAIN)
+            new_nodes.append(text_node)
+
+            try:
+                link_node = TextNode(links[i][0], TextType.LINK, links[i][1])
+                new_nodes.append(link_node)
+            except IndexError:
+                break
+    
+    return new_nodes
+
+def split_nodes_image(old_nodes:list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type is not TextType.PLAIN:
+            new_nodes.append(node)
+            continue
+        
+        images = extract_markdown_images(node.text)
+        texts = re.split(r"\!\[.*?\]\(.*?\)", node.text)
+
+        while "" in texts:
+            texts.remove("")
+
+        iters = max(len(images), len(texts))
+
+        for i in range(iters):
+            if node.text[0] == "!":
+                link_node = TextNode(images[i][0], TextType.IMAGE, images[i][1])
+                new_nodes.append(link_node)
+
+                try:
+                    text_node = TextNode(texts[i], TextType.PLAIN)
+                    new_nodes.append(text_node)
+                    continue
+                except IndexError:
+                    break
+            
+            text_node = TextNode(texts[i], TextType.PLAIN)
+            new_nodes.append(text_node)
+
+            try:
+                link_node = TextNode(images[i][0], TextType.IMAGE, images[i][1])
+                new_nodes.append(link_node)
+            except IndexError:
+                break
+    
+    return new_nodes
+
+def text_to_textnodes(text:str) -> list[TextNode]:
+    base_node = TextNode(text, TextType.PLAIN)
+
+    new_nodes = split_nodes_delimiter([base_node], "`", TextType.CODE)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    new_nodes = split_nodes_link(new_nodes)
+    new_nodes = split_nodes_image(new_nodes)
+
+    return new_nodes
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -5,7 +5,9 @@ from helpers import (
     text_node_to_html_node,
     split_nodes_delimiter,
     extract_markdown_images,
-    extract_markdown_links
+    extract_markdown_links,
+    split_nodes_link,
+    split_nodes_image,
 )
 
 
@@ -116,4 +118,47 @@ class TestExtractMarkdownLinks(unittest.TestCase):
             ("link", "https://facebook.com"),
         ]
         self.assertListEqual(expected, matches)
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    
+    def test_split_links(self):
+        old_nodes = [
+            TextNode(
+                "[to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+                TextType.PLAIN
+            ),
+            TextNode(
+                "This is a [game](https://friv.com) link",
+                TextType.PLAIN
+            )
+        ]
+        new_nodes = split_nodes_link(old_nodes)
+        self.assertListEqual(
+            [
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.PLAIN),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+                TextNode("This is a ", TextType.PLAIN),
+                TextNode("game", TextType.LINK, "https://friv.com"),
+                TextNode(" link", TextType.PLAIN),
+            ],
+            new_nodes
+        )
         
